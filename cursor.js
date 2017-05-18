52ad611c737bcd2b6f31235816000f505bf70456
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-18 23:55:10 +1200

    less closures and better edge cases

--- cursor.js
+++ cursor.js
@@ -3,37 +3,35 @@ module.exports = function (since, getMeta) {
   return function (cursor, live, reverse, format, test) {
     if(!format)
       format = function (_, value) { return value }
-    function get (offset, cb) {
-      since.once(function (_offset) {
-        if(offset == null)
-          offset = _offset
 
-        if(offset < 0)
+    return function (abort, cb) {
+      if(abort) return cb(abort)
+
+      if(test && cursor != null && !test(cursor))
+        return cb(true)
+
+      since.once(function (_offset) {
+        if(cursor == null)
+          cursor = _offset
+
+        if(cursor < 0)
           cb(true)
-        else if(offset <= _offset)
+        else if(cursor <= _offset)
           next()
         else if(live)
           since.once(next, false)
         else
           cb(true) //end of the stream
-      })
 
-      function next () {
-        getMeta(offset, cb)
-      }
-    }
-
-    return function (abort, cb) {
-      if(abort) return cb(abort)
-      var _cursor = cursor
-
-      if(test && !test(_cursor))
-        return cb(true)
-      get(_cursor, function (err, value, prev, next) {
-        //this should also handle ended state.
-        if(err) return cb(err)
-        cursor = reverse ? prev : next
-        cb(null, format(_cursor, value))
+        function next () {
+          getMeta(cursor, function (err, value, prev, next) {
+            //this should also handle ended state.
+            if(err) return cb(err)
+            var _cursor = cursor
+            cursor = reverse ? prev : next
+            cb(null, format(_cursor, value))
+          })
+        }
       })
     }
   }
